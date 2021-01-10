94279431186232db7d568addda0475ca8a491ce8
Crayon <614820984@qq.com>
2021-01-10 19:26:39 +0800

    [add] complete homework1 and write README.md

--- README.md
+++ README.md
@@ -1,1 +1,133 @@
 # 机器学习与数据挖掘
+
+## 项目名称：机器学习与数据挖掘大作业
+
+## 项目组成
+
+### 项目的人员组成与分工
+
+**组长：苏林斌 （1806300180）**
+
+**组员：欧阳宇航 （1806300189）**
+
+​			**陈名耀 （1806300193）**
+
+
+
+### 项目目录结构说明
+
+```markdown
+|-- data_mining
+    |-- .gitignore
+    |-- install.sh // 环境安装脚本
+    |-- README.md // 作业文档说明
+    |-- requirement.txt // 依赖文件
+    |-- .idea
+    |   |-- ...
+    |-- hw1 // 实验一
+    |   |-- main.py // 主程序
+    |   |-- data // 存放源数据的文件夹
+    |       |-- data01.xlsx // 模拟数据库数据的xlsx文件
+    |       |-- data02.txt // txt文本数据文件
+    |       |-- merged.csv // 经过数据清洗合并后生成的csv文件
+    |-- hw2 // 实验二
+    |-- hw3 // 实验三
+    |-- README.assets // 存放README文档相关资源的目录
+        |-- ...
+```
+
+
+
+## 作业选题
+
+- **实验一：《多源数据集成、清洗和统计》**
+- **实验二：《数据统计和可视化》**
+- **实验三：《k-means聚类算法》**
+
+
+
+## 运行环境
+
+三次实验都是使用python语言完成的
+
+python环境：python3.x
+
+所需依赖在requirement.txt中
+
+### Installation
+
+#### 0. 获取代码
+
+```bash
+git clone https://github.com/su614820984/data_mining
+```
+
+#### 1. python安装
+
+下载网址：  [https://www.python.org/downloads/](https://www.python.org/downloads/)
+
+#### 2. 创建虚拟环境
+
+python -m venv env
+
+#### 3. py环境切换
+
+windows
+
+```bash
+cd env/Scripts
+activate.bat
+cd ../../
+```
+
+linux
+
+```bash
+source env/Scripts/activate
+```
+
+#### 4. 依赖安装
+
+```bash
+pip install -r requirement.txt
+```
+
+
+
+#### * 也可以略过2~4步，直接使用install.sh脚本进行安装
+
+注：windows下可以使用git自带的bash命令行来运行sh脚本
+
+```bash
+./install.sh # 运行环境安装脚本
+```
+
+
+
+## 程序运行结果
+
+### 如何运行
+
+```bash
+python hw1/main.py # 实验一程序
+python hw2/main.py # 实验二程序
+python hw3/main.py # 实验三程序
+```
+
+### 实验一
+
+![1610266975730](README.assets/1610266975730.png)
+
+### 实验二
+
+
+
+### 实验三
+
+
+
+## 难题与解决
+
+
+
+## 总结
--- hw1/main.py
+++ hw1/main.py
@@ -8,20 +8,31 @@
 """
 import pandas as pd
 import numpy as np
-from numpy import nan
+import os
 
+BASE_PATH = os.path.dirname(__file__) or '.'
+print(BASE_PATH)
+# 带读取的数据的文件列表
 FILE_PATH = [
-    'data/data01.xlsx',
-    'data/data02.txt'
+    f'{BASE_PATH}/data/data01.xlsx',
+    f'{BASE_PATH}/data/data02.txt'
 ]
 
 
 def changeIDFormat(data):
+    """
+    该函数用来改变学生id格式，统一加上202000的前缀
+    :param data: type: dataframe 待处理的数据
+    """
     baseNum = 202000
     data['ID'] = data['ID'].map(lambda x: x + baseNum)
 
 
 def changeGenderFormat(data):
+    """
+    该函数将改变性别的格式，统一映射成boy和girl的格式
+    :param data: type: dataframe 待处理的数据
+    """
     gender_map = {
         'male': 'boy',
         'female': 'girl',
@@ -33,6 +44,10 @@ def changeGenderFormat(data):
 
 
 def changeHeightFormat(data):
+    """
+    该函数用来改变身高数据的格式，统一以m为单位，数值较小
+    :param data: type: dataframe 待处理的数据
+    """
     indexs = data[data['Height'] > 5].index
     data.loc[indexs, 'Height'] /= 100
 
@@ -40,7 +55,7 @@ def changeHeightFormat(data):
 def removeCnRows(data):
     """
     该函数用来删除成绩列有异常的行
-    :param data: type: dataframe 待处理的数据列表
+    :param data: type: dataframe 待处理的数据
     """
     # 1到5为100分制成绩 超过100分的行就是异常
     for i in range(1, 6):
@@ -50,7 +65,28 @@ def removeCnRows(data):
         data = data.drop(data[data[f'C{i}'] > 10].index)
 
 
+def changeConstitutionFormat(data):
+    """
+    该函数将体测的非数值型数据映射为数值型数据
+    :param data: type: dataframe 待处理的数据
+    """
+    constitution_map = {
+        'bad': 25,
+        'general': 50,
+        'good': 75,
+        'excellent': 100
+    }
+    data['Constitution'] = data['Constitution'].map(constitution_map)
+
+
 def checkData(xlsx, txt):
+    """
+    查验数据，以txt为基准数据，对于其中含有空值的行取xlsx中找到空缺的数据并补上，如果处理后还是存在空值，则删去这一行
+    从xlsx的不含有空值的数据中查找txt中没有的学生的信息，补充到txt数据中，并重新按学号排序，重置索引值
+    :param xlsx: type: dataframe xlsx表中的数据
+    :param txt: type: dataframe txt文件中的数据
+    :return: txt type: dataframe 处理后的txt数据
+    """
     # 获取列名
     cols = txt.columns
     # 包含空值的行
@@ -92,6 +128,12 @@ def checkData(xlsx, txt):
 
 
 def merge_data(xlsx, txt):
+    """
+    对数据进行清晰并合并
+    :param xlsx: type: dataframe xlsx表中的数据
+    :param txt: type: dataframe txt文件中的数据
+    :return: dataTable type: dataframe 合并好的数据
+    """
     # 改变xlsx中ID格式
     changeIDFormat(xlsx)
     # 改变txt中gender格式
@@ -101,11 +143,18 @@ def merge_data(xlsx, txt):
     dataTable = checkData(xlsx, txt)
     # 改变合并后数据的Height的格式 以m为单位
     changeHeightFormat(dataTable)
+    # 去除成绩有异常的行
     removeCnRows(dataTable)
+    # 将体测数据映射到数值型
+    changeConstitutionFormat(dataTable)
     return dataTable
 
 
 def get_data():
+    """
+    用来获取源数据，xlsx表中的数据用来模拟数据库中查出的数据
+    :return: dataTable type: dataframe 返回由源数据经过清洗合并后的数据
+    """
     # 读取excel表中的数据
     data_xlsx = pd.read_excel(FILE_PATH[0])
     # 删掉C10列的数据，因为都为空
@@ -120,11 +169,137 @@ def get_data():
     return dataTable
 
 
+def task1(data, decimal=2):
+    """
+    计算学生中家乡在Beijing的所有课程的平均成绩
+    :param data: type: dataframe
+    :param decimal: int 保留的小数点位数
+    :return: avg 均值
+    """
+    if not isinstance(decimal, int):
+        raise TypeError(f'Invalid type(need int but get {type(decimal)})')
+
+    total = 0
+    n = 0
+    for row in data.iloc:
+        # 家乡在Beijing的同学
+        if row['City'] == 'Beijing':
+            n += 1
+            # 5列（包含）之后的是学生成绩
+            for col in row[5:]:
+                total += col
+    # 求均值，n是学生数，10是成绩数目
+    avg = round(total / (n * 10), decimal)
+    return avg
+
+
+def task2(data):
+    """
+    统计家乡在广州，课程1在80分以上，且课程9在9分以上的男同学的数量
+    :param data: type: dataframe
+    :return: n 统计数
+    """
+    n = 0
+    for row in data.iloc:
+        # 家乡在Guangzhou 课程1成绩在80分以上 课程9在9分以上
+        if row['City'] == 'Guangzhou' and row['C1'] > 80 and row['C9'] > 9 and row['Gender'] == 'boy':
+            n += 1
+
+    return n
+
+
+def task3(data):
+    """
+    比较广州和上海两地女生的平均体能测试成绩，哪个地区的更强些
+    :param data: type: dataframe
+    :return: type: str 返回地区名
+    """
+    gz_total = 0
+    sh_total = 0
+    gz_n = 0
+    sh_n = 0
+
+    for row in data.iloc:
+        # 广州
+        if row['City'] == 'Guangzhou':
+            gz_total += row[-1]
+            gz_n += 1
+        # 上海
+        if row['City'] == 'Shanghai':
+            sh_total += row[-1]
+            sh_n += 1
+        else:
+            pass
+
+    gz_avg = 0
+    sh_avg = 0
+    gz_avg = gz_total / gz_n
+    sh_avg = sh_total / sh_n
+
+    if gz_avg > sh_avg:
+        return '广州'
+    elif gz_avg < sh_avg:
+        return '上海'
+    elif gz_avg == sh_avg:
+        return '一样'
+    else:
+        pass
+
+
+def get_cov(data):
+    """
+    计算得到协方差矩阵
+    :param data: type: numpy.array 数组类型，带计算的矩阵
+    :return: cov_mat type: numpy.array 返回协方差矩阵
+    """
+    # 计算mean 和 std
+    total = 0
+    for v in data:
+        total += v
+    mean = total / len(data)
+    total = 0
+    for v in data:
+        total += ((v - mean) ** 2)
+    std = (total / (len(data) - 1)) ** 0.5
+
+    # 协方差矩阵
+    cov_mat = []
+    for i in range(len(data)):
+        cov_mat.append((data[i] - mean) / std)
+
+    return np.array(cov_mat)
+
+
+def task4(data, decimal=2):
+    """
+    计算九门课的成绩分别与体能成绩计算相关性
+    :param data: type: dataframe
+    :param decimal: 保留几位小数
+    """
+    if not isinstance(decimal, int):
+        raise TypeError(f'Invalid type(need int but get {type(decimal)})')
+
+    # 体测成绩
+    constitution_cov = get_cov(np.array(data['Constitution']))
+    # print(constitution_cov)
+    for i in range(0, 9):
+        correlation = 0
+        ci_cov = get_cov(np.array(data[f'C{i + 1}']))
+        # 点乘计算，对应位计算并相加
+        for idx, v in enumerate(constitution_cov):
+            correlation += ci_cov[idx] * v
+        print(f'C{i + 1}与Constitution相关性：{round(correlation, decimal)}')
+
+
 def main():
     # 获取数据
     data = get_data()
     # 保存数据为csv文件 不保留索引值
-    data.to_csv('data/merged.csv', index=0)
+    data.to_csv(f'{BASE_PATH}/data/merged.csv', index=0)
+    print(f'学生中家乡在Beijing的所有课程的平均成绩：【{task1(data)}】')
+    print(f'学生中家乡在广州，课程1在80分以上，且课程9在9分以上的男同学的数量：【{task2(data)}】')
+    print(f'比较广州和上海两地女生的平均体能测试成绩，哪个地区的更强些：【{task3(data)}】')
+    task4(data)
 
 
 if __name__ == "__main__":
