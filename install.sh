a972038a1d48f918725c9c1ad49210465fc78d84
Crayon <614820984@qq.com>
2021-01-10 19:40:15 +0800

    [modify] modify install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo 'Hello'
+echo '===create a new python env==='
+# create a new python env named "env"
+python -m venv env
+echo '===create env successfully==='
+
+# activate the new env
+source env/Scripts/activate
+
+echo '===start installing requirement==='
+# install requirement
+pip install -r requirement.txt
+
+echo 'successful'
