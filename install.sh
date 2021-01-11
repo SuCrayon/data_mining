aaa85da8e9a9bed6bf7076480c329404c807571a
Crayon <614820984@qq.com>
2021-01-11 15:25:03 +0800

    [modify] modify install.sh

--- install.sh
+++ install.sh
@@ -3,22 +3,22 @@ os=`uname -o`
 
 echo '===create a new python env==='
 # create a new python env named "env"
-# python -m venv env
+python -m venv env
 echo '===create env successfully==='
 
 # activate the new env
 if [ ${os} == 'Msys' ]
 then
-	# source env/Scripts/activate
+	source env/Scripts/activate
 	echo 'Windows OS'
 elif [ ${os} == 'GNU/Linux' ]
 then
-	# source env/bin/activate
+	source env/bin/activate
 	echo 'Linux OS'
 fi
 
 echo '===start installing requirements==='
 # install requirement
-# pip install -r requirements.txt
+pip install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 
 echo 'successful'
